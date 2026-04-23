a243190d518acb8a9ef78a324f61b53b2d7f89cd
Matteo Benvenuti <matteo.benvenuti3310@icloud.com>
2026-04-23 12:53:27 -0700

    Gradebook Manager

--- Gradebook Manager/main.cpp
+++ Gradebook Manager/main.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main() {
     Student testStudent(123, "Test");
 
-    testStudent.addScore(99.9);
-
     testStudent.displayInfo();
 
     return 0;
--- Gradebook Manager/student.cpp
+++ Gradebook Manager/student.cpp
@@ -16,18 +16,69 @@ void Student::addScore(double score)
     }
 }
 
+double Student::getAverage() const
+{
+    if(record.numScores == 0) {
+        return 0.0; 
+    }
+
+    double scoresSum = 0.0;
+
+    for(int i = 0; i < record.numScores; i++) {
+        scoresSum += record.scores[i];
+    }
+
+    return scoresSum / record.numScores;
+}
+
+std::string Student::getLetterGrade() const
+{
+    if(record.numScores == 0) {
+        return "No scores inserted";
+    }
+
+    if(getAverage() >= 90.0) {
+        return "A";
+    }
+
+    else if(getAverage() >= 80.0) {
+        return "B";
+    }
+
+    else if(getAverage() >= 70) {
+        return "C";
+    }
+
+    else if(getAverage() >= 60 ) {
+        return "D";
+    }
+
+    return "F";
+}
+
 void Student::displayInfo() const
 {
     std::cout << "Name: " << record.name << std::endl;
     std::cout << "Id: " << record.studentId << std::endl;
 
-    for(int i = 0; i < record.numScores; i++) {
-        std::cout << "Scores: " << record.scores[i];
-
-        if(i != record.numScores - 1) {
-            std::cout << " ";
+    if(record.numScores != 0) {
+        for(int i = 0; i < record.numScores; i++) {
+            std::cout << "Scores: " << record.scores[i];
+    
+            if(i != record.numScores - 1) {
+                std::cout << " ";
+            }
         }
+        
+        std::cout << std::endl;
     }
 
-    std::cout << std::endl;
+
+    std::cout << "Average: " << getAverage() << std::endl;
+    std::cout << "Letter Grade: " << getLetterGrade() << std::endl;
+}
+
+StudentRecord Student::getRecord() const
+{
+    return record;
 }
