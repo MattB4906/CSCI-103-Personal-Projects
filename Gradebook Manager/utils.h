fde8ff8b6553d16a5f0bc16fbc52ba5ada08c8ec
Matteo Benvenuti <matteo.benvenuti3310@icloud.com>
2026-04-24 11:13:35 -0700

    Gradebook Manager

---
+++ Gradebook Manager/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string>
+
+double calculatedWeightedAverage(double scores[], double weight[], int size);
+double recursiveSum(double scores[], int size);
+std::string assignLetterGrade(double avg);
+bool isValidScore(double score);
+void clearInputBuffer();
+void printDivider(char c, int length);
+
+#endif
