3e8fd628bde5f81e56e7a9f03bbba8969945f418
Matteo Benvenuti <matteo.benvenuti3310@icloud.com>
2026-04-23 18:32:45 -0700

    Gradebook Manager

---
+++ Gradebook Manager/gradebook.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include "gradebook.h"
+
+Gradebook::Gradebook(int intialCapacity)
+{
+    capacity = intialCapacity;
+    
+    courses = new Course*[capacity];
+    numCourses = 0;
+}
+
+Gradebook::~Gradebook()
+{
+    delete[] courses;
+}
+
+void Gradebook::addCourse(Course* c)
+{
+    courses[numCourses] = c;
+    numCourses++;
+}
+
+void Gradebook::removeCourse(std::string courseCode)
+{
+    int index = -1;
+
+    for(int i = 0; i < numCourses; i++) {
+        if(courses[i]->getCourseCode() == courseCode) {
+            index = i;
+            break;
+        }
+    }
+
+    if(index == -1) {
+        std::cout << "Student not found" << std::endl;
+        return;
+    }
+
+    for(int i = index; i < numCourses - 1; i++) {
+        courses[i] = courses[i + 1];
+    }
+
+    numCourses--;
+}
+
+Course* Gradebook::findCourse(std::string courseCode) const
+{
+    for(int i = 0; i < numCourses; i++) {
+        if(courseCode == courses[i]->getCourseCode()) {
+            return courses[i];
+        }
+    }
+
+    return nullptr;
+}
+
+void Gradebook::displayAllCourses() const
+{
+    for(int i = 0; i < numCourses; i++) {
+        std::cout << "Course Code: " << courses[i]->getCourseCode() << std::endl;
+        std::cout << "Course Name: " << courses[i]->getCourseName() << std::endl;
+        courses[i]->displayRoster();
+    }
+}
+
+void Gradebook::saveToFile(std::string filename) const
+{
+    std::ofstream ofile(filename);
+
+    if(ofile.is_open()) {
+        for(int i = 0; i < numCourses; i++) {
+            ofile << courses[i]->getCourseCode() << ", " << courses[i]->getCourseName() << ", ";
+        }
+
+        ofile.close();
+
+        return;
+    }
+
+    else {
+        std::cout << "Error writing in file" << std::endl;
+        return;
+    }
+}
+
+void Gradebook::loadFromFile(std::string filename)
+{
+    std::ifstream ifile(filename);
+
+    if(ifile.is_open()) {
+        for(int i = 0; i < numCourses; i++) {
+            std::getline(ifile, courses[i], ", ");
+        }
+        
+        ifile.close();
+
+        return;
+    }
+
+    else {
+        std::cout << "Error opening file" << std::endl;
+        return;
+    }
+}
---
+++ Gradebook Manager/gradebook.h
@@ -0,0 +1,24 @@
+#ifndef GRADEBOOK_H
+#define GRADEBOOK_H
+
+#include "course.h"
+
+class Gradebook {
+    private:
+        Course** courses;
+        int numCourses;
+        int capacity;
+
+    public:
+        Gradebook(int initialCapacity);
+        ~Gradebook();
+
+        void addCourse(Course* c);
+        void removeCourse(std::string courseCode);
+        Course* findCourse(std::string courseCode) const;
+        void displayAllCourses() const;
+        void saveToFile(std::string filename) const;
+        void loadFromFile(std::string filename);
+};
+
+#endif
